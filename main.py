9d1218276c0942425aaf70e6a2a6087809d5a1fe
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-20 20:11:41 +0530

    Draw square and made it move with arrow keys

--- main.py
+++ main.py
@@ -6,14 +6,33 @@ pygame.init()
 
 WIDTH = 500
 HEIGHT = 400
-WINDOW =  pygame.display.set_mode((WIDTH, HEIGHT))
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
+clock = pygame.time.Clock()
 
-pygame.display.set_caption("Hello World")
+x, y = 50, 40
+vel = 3
 
-while True:
+run = True
+while run:
+	clock.tick(FPS)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
+			run = False
 			pygame.quit()
 			sys.exit()
+
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_LEFT]:
+	    x -= vel
+	elif keys[pygame.K_RIGHT]:
+	 	x += vel
+	elif keys[pygame.K_UP]:
+		y -= vel
+	elif keys[pygame.K_DOWN]:
+		y += vel
+
+	WINDOW.fill((0, 65, 123))
+	pygame.draw.rect(WINDOW, (204, 204, 204), (x, y, 100, 100))
 
 	pygame.display.update()
