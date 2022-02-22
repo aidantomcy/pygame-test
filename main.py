1e2a1ec771f3600d12f608ac715eea089e88c7d7
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-22 16:40:30 +0530

    Refactor code

--- main.py
+++ main.py
@@ -10,29 +10,35 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 clock = pygame.time.Clock()
 
-x, y = 50, 40
-vel = 3
 
-run = True
-while run:
-	clock.tick(FPS)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-			pygame.quit()
-			sys.exit()
+def main():
+	x, y = 50, 40
+	vel = 3
 
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_LEFT]:
-	    x -= vel
-	elif keys[pygame.K_RIGHT]:
-	 	x += vel
-	elif keys[pygame.K_UP]:
-		y -= vel
-	elif keys[pygame.K_DOWN]:
-		y += vel
+	run = True
+	while run:
+		clock.tick(FPS)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+				pygame.quit()
+				sys.exit()
 
-	WINDOW.fill((0, 65, 123))
-	pygame.draw.rect(WINDOW, (204, 204, 204), (x, y, 100, 100))
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_LEFT]:
+		    x -= vel
+		elif keys[pygame.K_RIGHT]:
+		 	x += vel
+		elif keys[pygame.K_UP]:
+			y -= vel
+		elif keys[pygame.K_DOWN]:
+			y += vel
 
-	pygame.display.update()
+		WINDOW.fill((0, 65, 123))
+		pygame.draw.rect(WINDOW, (204, 204, 204), (x, y, 100, 100))
+
+		pygame.display.update()
+
+
+if __name__ == '__main__':
+	main()
